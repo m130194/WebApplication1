4557e6e91f0b5469302a404acf137dfa5e8e6d04
unknown <M130194@tafe.wa.edu.au>
2026-09-04 14:32:25 +0800

    week 7 part 18 create BlogPostUpdateViewModel for full update operation

---
+++ WebApplication1/ViewModels/BlogPostUpdateViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.ViewModels
+{
+    public sealed class BlogPostUpdateViewModel
+    {
+        [Required]
+        public string Title { get; set; } = string.Empty;
+        [Required]
+        public string Content { get; set; } = string.Empty;
+        [Required]
+        public string Category { get; set; } = string.Empty;
+        public List<string> Tags { get; set; } = [];
+        public bool IsPublished { get; set; }
+
+    }
+}
