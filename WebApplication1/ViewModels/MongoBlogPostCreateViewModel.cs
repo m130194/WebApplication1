2d2d3bc2818b89a44177ddf44c7a099ff5eace0d
unknown <M130194@tafe.wa.edu.au>
2026-09-04 10:13:33 +0800

    build Week 6 Part 10 Create ViewModel for MongoDB Posts

---
+++ WebApplication1/ViewModels/MongoBlogPostCreateViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace WebApplication1.ViewModels
+{
+    public sealed class MongoBlogPostCreateViewModel
+    {
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; } = string.Empty;
+        [Required]
+        [StringLength(2000, MinimumLength = 10)]
+        public string Content { get; set; } = string.Empty;
+        [Required]
+        public string AuthorName { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string AuthorEmail { get; set; } = string.Empty;
+        public List<string> Tags { get; set; } = [];
+        public bool IsPublished { get; set; }
+    }
+}
