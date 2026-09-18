95a3ae1e4e3fe4e62673a96441a4dd3a5c913c8e
unknown <M130194@tafe.wa.edu.au>
2026-09-18 15:15:50 +0800

    Week 9 Part 20: Create Security Test Controller in Controllers

---
+++ WebApplication1/Controllers/SecurityDemoController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+
+    [ApiController]
+    [Route("api/security")]
+    public sealed class SecurityDemoController : ControllerBase
+    {
+        [AllowAnonymous]
+        [HttpGet("public")]
+        public IActionResult Public()
+        {
+            return Ok(new
+            {
+                message =
+            "This endpoint is public."
+            });
+        }
+        [Authorize]
+        [HttpGet("profile")]
+        public IActionResult Profile()
+        {
+            return Ok(new
+            {
+                username =
+            User.Identity?.Name,
+                authenticated =
+            User.Identity?.IsAuthenticated,
+                message =
+            "You successfully authenticated."
+            });
+        }
+
+    }
+}
