6b7e45468800df068de08869ac7b6e9b045c06de
unknown <M130194@tafe.wa.edu.au>
2026-09-18 11:48:19 +0800

    Week 8 Part 21: Create the MongoActivityLogService in Services

---
+++ WebApplication1/Services/MongoActivityLogService.cs
@@ -0,0 +1,34 @@
+﻿using WebApplication1.Configuration;
+using WebApplication1.Models;
+using WebApplication1.ViewModels;
+using MongoDB.Driver;
+
+namespace WebApplication1.Services
+{
+    public sealed class MongoActivityLogService
+    {
+        private readonly
+ IMongoCollection<ActivityLogDocument> _activityLogs;
+        public MongoActivityLogService(
+        IMongoClient mongoClient,
+        MongoDbSettings settings)
+        {
+            IMongoDatabase database =
+            mongoClient.GetDatabase(
+            settings.DatabaseName);
+            _activityLogs =
+            database.GetCollection<ActivityLogDocument>(
+            settings.ActivityLogCollectionName);
+        }
+        public async Task InsertAsync(
+        ActivityLogDocument log,
+        CancellationToken cancellationToken = default)
+        {
+            await _activityLogs.InsertOneAsync(
+            log,
+            cancellationToken:
+            cancellationToken);
+        }
+
+    }
+}
