6e110580436bf2796a03062422e2b8851d7d79b9
unknown <M130194@tafe.wa.edu.au>
2026-09-18 13:27:35 +0800

    Week 8 Part 36: Create a Projection ViewModel

---
+++ WebApplication1/ViewModels/ActivityLogSummaryViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.ViewModels
+{
+    public sealed class ActivityLogSummaryViewModel
+    {
+        public string OperationType { get; set; }
+            = string.Empty;
+        public string? BlogPostId { get; set; }
+        public string? Title { get; set; }
+        public string Message { get; set; }
+        = string.Empty;
+        public DateTime CreatedAtUtc { get; set; }
+    }
+}
