e612a791778388c87fba18838d61830e910e3ec3
unknown <M130194@tafe.wa.edu.au>
2026-09-18 11:36:29 +0800

    Week 8 Part 17: Update Program.cs Configuration to include ActivityLogCollectionName

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 //Register MongoDB in Program.cs
 //MongoClient manages the connection to the MongoDB deployment
+//Configuration in MongoDbSettings.cs
 string connectionString =
  builder.Configuration["MongoDb:ConnectionString"]
  ?? throw new InvalidOperationException(
@@ -18,11 +19,16 @@ string collectionName =
  builder.Configuration["MongoDb:CollectionName"]
  ?? throw new InvalidOperationException(
  "MongoDb:CollectionName has not been configured.");
+string activityLogCollectionName =
+ builder.Configuration["MongoDb:ActivityLogCollectionName"]
+ ?? throw new InvalidOperationException(
+ "MongoDb:ActivityLogCollectionName has not been configured.");
 MongoDbSettings mongoDbSettings = new()
 {
     ConnectionString = connectionString,
     DatabaseName = databaseName,
-    CollectionName = collectionName
+    CollectionName = collectionName,
+    ActivityLogCollectionName = activityLogCollectionName
 };
 builder.Services.AddSingleton(mongoDbSettings);
 builder.Services.AddSingleton<IMongoClient>(
