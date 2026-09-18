76d9eb6e3bbaa4f500543f6d47f40b4d9c216e46
unknown <M130194@tafe.wa.edu.au>
2026-09-18 11:40:21 +0800

    Week 8 Part 18: Create ActivityLogDocument Model

---
+++ WebApplication1/Models/ActivityLogDocument.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace WebApplication1.Models
+{
+    public sealed class ActivityLogDocument
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        [BsonElement("operationType")]
+        public string OperationType { get; set; }
+        = string.Empty;
+        [BsonElement("blogPostId")]
+        public string? BlogPostId { get; set; }
+        [BsonElement("title")]
+        public string? Title { get; set; }
+        [BsonElement("message")]
+        public string Message { get; set; }
+        = string.Empty;
+        [BsonElement("createdAtUtc")]
+        public DateTime CreatedAtUtc { get; set; }
+        [BsonElement("expiresAtUtc")]
+        public DateTime ExpiresAtUtc { get; set; }
+    }
+}
