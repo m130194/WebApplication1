d15f0744091d9d3cf43139a6ed5b3604c4c12531
unknown <M130194@tafe.wa.edu.au>
2026-09-25 11:35:22 +0800

    Week 10 Part 31: Write a demo controller unit test

---
+++ WebApplication1.Tests/SecurityDemoControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApplication1.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Tests
+{
+    public sealed class SecurityDemoControllerTests
+    {
+        [Fact]
+        public void Public_ReturnsOkResult()
+        {
+            // Arrange
+            SecurityDemoController controller =
+            new();
+            // Act
+            IActionResult result =
+            controller.Public();
+            // Assert
+            OkObjectResult okResult =
+            Assert.IsType<OkObjectResult>(
+            result);
+            Assert.Equal(
+            200,
+            okResult.StatusCode);
+        }
+    }
+}
