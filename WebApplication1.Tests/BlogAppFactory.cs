e682d03d175cfc074796a662e40d82ae15db165c
unknown <M130194@tafe.wa.edu.au>
2026-09-25 11:58:26 +0800

    Week 10 Part 40: Create a WebApplicationFactory

---
+++ WebApplication1.Tests/BlogAppFactory.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using static System.Net.WebRequestMethods;
+
+
+namespace WebApplication1.Tests
+{
+
+    //This is an integration test
+    //starts your app in a test environment
+    //without requiring app to be manually started from Visual Studio, this allows tests to send HTTP requests through:
+    //ASP.NET Core routing
+    //Middleware
+    //Authentication
+    //Authorization
+    //Controllers
+
+    public sealed class BlogAppFactory
+ : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(
+        IWebHostBuilder builder)
+        {
+            builder.UseEnvironment(
+            "Testing");
+            builder.ConfigureAppConfiguration(
+            (context, configuration) =>
+            {
+                Dictionary<string, string?>
+     settings =
+    new()
+    {
+        [
+     "MongoDb:ConnectionString"
+     ] =
+     "mongodb://localhost:27017",
+        [
+     "MongoDb:DatabaseName"
+     ] =
+     "BlogDb",
+        [
+     "MongoDb:CollectionName"
+     ] =
+     "BlogPosts",
+        [
+     "MongoDb:ActivityLogCollectionName"
+     ] =
+     "ActivityLogs",
+        [
+     "BasicAuth:AdminUsername"
+     ] =
+     "admin",
+        [
+     "BasicAuth:AdminPassword"
+     ] =
+     "TestAdminPassword",
+        [
+     "BasicAuth:EditorUsername"
+     ] =
+     "editor",
+        [
+     "BasicAuth:EditorPassword"
+     ] =
+     "TestEditorPassword"
+    };
+                configuration
+     .AddInMemoryCollection(
+     settings);
+            });
+        }
+    }
+}
